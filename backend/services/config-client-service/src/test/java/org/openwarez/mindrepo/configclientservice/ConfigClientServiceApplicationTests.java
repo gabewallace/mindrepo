337e50d0dc605eea984900aaff6dad3c585febe8
gabewallace <gabriel.wallace.us@gmail.com>
2023-10-29 22:36:44 -0500

    Move to services directory

---
+++ backend/services/config-client-service/src/test/java/org/openwarez/mindrepo/configclientservice/ConfigClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.openwarez.mindrepo.configclientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
